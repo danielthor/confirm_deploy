298fc32b632038643e8218ef3df88f16fe910d16
Daniel Thor <daniel.thor@gmail.com>
2012-12-13 22:45:29 +0100

    simple confirm task

--- lib/confirm_deploy.rb
+++ lib/confirm_deploy.rb
@@ -1,3 +1,14 @@
-module ConfirmDeploy
-  # Your code goes here...
+require 'capistrano/configuration'
+
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :deploy do
+    desc "Prompts user to confirm deploy"
+    task :confirm do
+      unless Capistrano::CLI.ui.ask("  Are you sure you want to deploy? (yes): ") == 'yes'
+        logger.important "Deploy cancelled!"
+        exit
+      end
+    end
+    before "deploy:update_code", "deploy:confirm"
+  end
 end
