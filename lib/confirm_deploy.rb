5d91de6a443912d2e32ff4be9706cfefc7ff57b7
Daniel Thor <daniel.thor@gmail.com>
2012-12-16 17:25:21 +0100

    custom confirmation answer

--- lib/confirm_deploy.rb
+++ lib/confirm_deploy.rb
@@ -5,9 +5,10 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deploy do
     desc "Prompts user to confirm deploy"
     task :confirm do
-      message = fetch(:confirm_message, "  Are you sure you want to deploy? (yes): ")
+      message = fetch(:confirm_message, "Are you sure you want to deploy?")
+      answer = fetch(:confirm_answer, "yes")
 
-      unless Capistrano::CLI.ui.ask(message) == 'yes'
+      unless Capistrano::CLI.ui.ask("  #{message} (#{answer}): ") == answer
         logger.important "Deploy cancelled!"
         abort
       end
